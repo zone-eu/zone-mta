ac25fa24023be8a4e4c2cb5c4ef9fd1c719d74d8
Andris Reinman <andris@zone.ee>
2016-08-16 15:54:14 +0300

    do not wait 5 sec after queue is finished

--- sender.js
+++ sender.js
@@ -89,7 +89,7 @@ function sender() {
 
             if (!delivery || !delivery.id) {
                 emptyChecks++;
-                return setTimeout(sendNext, Math.min(Math.power(emptyChecks, 2), 1000) * 10);
+                return setTimeout(sendNext, Math.min(Math.pow(emptyChecks, 2), 1000) * 10);
             }
             emptyChecks = 0;
 
