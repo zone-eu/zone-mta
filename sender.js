c73d1153f4e53087054fef7c3e647eceb78863d1
Andris Reinman <andris@zone.ee>
2016-08-19 11:25:28 +0300

    u

--- sender.js
+++ sender.js
@@ -61,10 +61,6 @@ function sendCommand(cmd, callback) {
     }
 
     Object.keys(cmd).forEach(key => data[key] = cmd[key]);
-
-    console.log('C: [%s/%s] sending', zone.name, process.pid);
-    console.log(data);
-
     responseHandlers.set(id, callback);
     queueClient.send(data);
 }
@@ -86,10 +82,6 @@ queueClient.connect(err => {
     queueClient.onData = (data, next) => {
         let callback;
         if (responseHandlers.has(data.req)) {
-
-            console.log('C: [%s/%s] received', zone.name, process.pid);
-            console.log(data);
-
             callback = responseHandlers.get(data.req);
             responseHandlers.delete(data.req);
             setImmediate(() => callback(data.error ? data.error : null, !data.error && data.response));
@@ -99,12 +91,6 @@ queueClient.connect(err => {
 
     // Notify the server about the details of this client
     queueClient.send({
-        cmd: 'HELLO',
-        zone: zone.name,
-        id: clientId
-    });
-    console.log('C: [%s/%s] received', zone.name, process.pid);
-    console.log({
         cmd: 'HELLO',
         zone: zone.name,
         id: clientId
