636c0f724f1cea2d7b64796f3994b70f31c754aa
Filippo Verni <filippo.verni@qboxmail.it>
2026-03-02 12:44:16 +0100

    config: make bounce rules file path configurable

--- config/default.js
+++ config/default.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
     // If started as root then privileges are dropped after all ports are bound
     //user: 'nobody',
@@ -514,6 +516,9 @@ module.exports = {
         }
         */
     },
+
+    // Bounce rules file path
+    bouncesFile: path.join(__dirname, 'bounces.txt'),
 
     pluginsPath: './plugins',
     corePluginsPath: './plugins'
